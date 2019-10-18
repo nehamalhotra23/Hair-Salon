bf2f0f5116133fa8ac5f6e82104efff140e4650b
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-10-18 09:21:57 -0700

    add list of stylists

---
+++ HairSalon/Controllers/StylistController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HairSalon.Controllers
+{
+  public class StylistController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public StylistController(HairSalonContext db)
+    {
+      _db = db;
+    }
+            
+    [HttpGet]    
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylist.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylist.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details(int id)
+    {
+     Stylist thisStylist = _db.Stylist.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+  }
+
+}  
